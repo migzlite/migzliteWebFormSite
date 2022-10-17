ac7b47b17d98fbcd61800305b82799b8493e66ca
mchramirez <mchramirez@MCHRAMIREZ03>
2022-10-17 15:00:56 +0800

    Add project files.

---
+++ migzliteWebFormSite/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(migzliteWebFormSite.Startup))]
+namespace migzliteWebFormSite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
